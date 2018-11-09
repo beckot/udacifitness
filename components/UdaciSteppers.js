fb3c6fd48a6dd3783dd9a3f293c18e179f2afbc2
Otto Becker <otto.becker@gmail.com>
2018-11-09 13:56:33 +0200

    feat: functioning styled buttons

--- components/UdaciSteppers.js
+++ components/UdaciSteppers.js
@@ -1,16 +1,25 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform } from 'react-native'
 import { FontAwesome, Entypo } from '@expo/vector-icons';
+import { white, purple } from '../utils/colors'
 
 export default function UdacitySteppers({ max, unit, step, value, onIncrement, onDecrement }) {
 
     return (
-        <View>
-            <View>
-                <TouchableOpacity onPress={onDecrement} >
+        <View style={[styles.row, { justifyContent: 'space-between'}]}>
+            <View style={ {flexDirection: 'row'}}>
+                <TouchableOpacity 
+                    style={ Platform.OS === 'ios' 
+                        ? [styles.iosBtn, { borderTopRightRadius: 0, borderBottomRightRadius: 0 }] 
+                        : [styles.androidBtn, , { borderTopRightRadius: 0, borderBottomRightRadius: 0 }] }
+                    onPress={onDecrement} >
                     <FontAwesome name='minus' size={30} color={'black'} />
                 </TouchableOpacity>
-                <TouchableOpacity onPress={onIncrement} >
+                <TouchableOpacity 
+                    style={ Platform.OS === 'ios' 
+                    ? [styles.iosBtn, { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }] 
+                    : [styles.androidBtn, , { borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }] }
+                    onPress={onIncrement} >
                     <FontAwesome name='plus' size={30} color={'black'} />
                 </TouchableOpacity>
             </View>
@@ -22,3 +31,29 @@ export default function UdacitySteppers({ max, unit, step, value, onIncrement, o
     )
 
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+    },
+    iosBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3,
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+    androidBtn: {
+        backgroundColor: white,
+        borderColor: purple,
+        borderWidth: 1,
+        borderRadius: 3, 
+        padding: 5,
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+})
