d1b9b42ae1273802dbd53cf07f6b1c33617c7806
Otto Becker <otto.becker@nortal.com>
2018-11-12 10:30:54 +0200

    WIP calendar history

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import { View } from 'react-native'
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import AddEntry from './components/AddEntry'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './reducers'
+import History from './components/History'
+import reducer from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}> 
+          <History />
           <AddEntry />
         </View>
       </Provider>
